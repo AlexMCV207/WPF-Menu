dd08c11675fa803c6f488ab81d0af5f3872dc8fd
AlexMCV207 <113392257+AlexMCV207@users.noreply.github.com>
2025-05-12 18:25:20 +0200

    Add files via upload

--- InputWindow.xaml.cs
+++ InputWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace WpfApp1
 {
@@ -18,70 +22,240 @@ namespace WpfApp1
     /// Logika interakcji dla klasy InputWindow.xaml
     /// </summary>
     
-    class Osoba
-    {
-        public string? u_pesel { get; set; }
-        public string? u_name { get; set; }
-        public string? u_sname { get; set; }
-        public string? u_lname { get; set; }
-        public string? u_date { get; set; }
-        public string? u_phone { get; set; }
-        public string? u_adress { get; set; }
-        public string? u_city { get; set; }
-        public string? u_postcode { get; set; }
-    }
+    
     public partial class InputWindow : Window
     {
         public InputWindow()
         {
             InitializeComponent();
         }
-        Osoba os = new Osoba();
+        public bool InputDataReturned = false;
+        Color red = Colors.Red;
+        Color white = Colors.White;
+        Color black = Colors.Black;
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-            os.u_pesel = u_pesel.Text;
-            os.u_name = u_name.Text;
-            os.u_sname = u_sname.Text;
-            os.u_lname = u_lname.Text;
-            os.u_date = u_date.Text;
-            os.u_phone = u_phone.Text;
-            os.u_adress = u_adress.Text;
-            os.u_city = u_city.Text;
-            os.u_postcode = u_postcode.Text;
 
-            if (os.u_pesel != "" || os.u_name != "" || os.u_sname != "" || os.u_lname != "" || os.u_date != "" || os.u_phone != "" || os.u_adress != "" || os.u_city != "" || os.u_postcode != "")
+            if (u_pesel.Text != "" || u_name.Text != "" || u_sname.Text != "" || u_lname.Text != "" || u_date.Text != "" || u_phone.Text != "" || u_address.Text != "" || u_city.Text != "" || u_postcode.Text != "")
             {
                 MessageBoxResult result = MessageBox.Show("Czy na pewno chcesz zamknąć to okno?","",MessageBoxButton.YesNo);
                 if (result == MessageBoxResult.Yes)
                 {
-                    MainWindow mW = new MainWindow();
-                    mW.Show();
                     this.Close();
                 }
             }
             else
             {
-                MainWindow mW = new MainWindow();
-                mW.Show();
                 this.Close();
             }
         }
 
         private void OK_Click(object sender, RoutedEventArgs e)
         {
-            os.u_pesel = u_pesel.Text;
-            os.u_name = u_name.Text;
-            os.u_sname = u_sname.Text;
-            os.u_lname = u_lname.Text;
-            os.u_date = u_date.Text;
-            os.u_phone = u_phone.Text;
-            os.u_adress = u_adress.Text;
-            os.u_city = u_city.Text;
-            os.u_postcode = u_postcode.Text;
+            string dzien = "00";
+            string miesiac = "00";
+            string rok = "0000";
 
-            if(os.u_pesel != "" && os.u_name != "" && os.u_lname != "" && os.u_date != "" && os.u_adress != "" && os.u_city != "" && os.u_postcode != "")
+            bool isPeselFilled = false;
+            bool isPeselCorrect = false;
+            bool isNameCorrect = false;
+            bool isLnameCorrect = false;
+            bool isDateCorrect = false;
+            bool isAddressCorrect = false;
+            bool isCityCorrect = false;
+            bool isPostcodeCorrect = false;
+
+            if (u_date.Text != "" && u_date.Text.Length >= 8)
             {
+                u_date.Background = new SolidColorBrush(white);
+                u_date.Foreground = new SolidColorBrush(black);
+                string date = u_date.Text;
+                date.Replace(" ", "");
+                date.Replace("/", ".");
+                if(date.Length == 8)
+                {
+                    date = date.Substring(0,2) + "." + date.Substring(2,2) + "." + date.Substring(4,4);
+                }
+                dzien = date.Substring(0, 2);
+                miesiac = date.Substring(3, 2);
+                rok = date.Substring(6, 4);
+                u_date.Text = date;
+                isDateCorrect = true;
+            } else {
+                u_date.Background = new SolidColorBrush(red);
+                u_date.Foreground = new SolidColorBrush(white);
+                isDateCorrect = false;
+            }
+            if(u_pesel.Text != "")
+            {
+                string pesel = u_pesel.Text;
+                isPeselFilled = true;
+                if(pesel.Length != 11 || int.TryParse(pesel,out _) != false)
+                {
+                    u_pesel.Background = new SolidColorBrush(red);
+                    u_pesel.Foreground = new SolidColorBrush(white);
+                    isPeselCorrect = false;
+                }
+                else
+                {
+                    int[] wagi = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+                    int sumaK = 0;
+                    for(int i = 0; i < 10; i++)
+                    {
+                        sumaK += (pesel[i] * wagi[i]);
+                    }
+                    sumaK = (10 - (sumaK % 10) % 10);
+
+                    if (int.Parse(rok) > 1799 && int.Parse(rok) < 1900)
+                    {
+                        miesiac = (int.Parse(miesiac) + 80).ToString();
+                    } else if (int.Parse(rok) > 1899 && int.Parse(rok) < 2100)
+                    {
+                        miesiac = (int.Parse(rok) + 20).ToString();
+                    }
+
+                    if (dzien != pesel.Substring(4, 2) && miesiac != pesel.Substring(2, 2) && rok.Substring(2, 2) != pesel.Substring(0, 2) && sumaK.ToString() != pesel.Substring(10,1))
+                    {
+                        u_pesel.Background = new SolidColorBrush(red);
+                        u_pesel.Foreground = new SolidColorBrush(white);
+                        isPeselCorrect = false;
+                    } else
+                    {
+                        u_pesel.Background = new SolidColorBrush(white);
+                        u_pesel.Foreground = new SolidColorBrush(black);
+                        isPeselCorrect = true;
+                    }
+                }
+            } else
+            {
+                u_pesel.Background = new SolidColorBrush(red);
+                u_pesel.Foreground = new SolidColorBrush(white);
+                isPeselFilled = false;
+            }
+            if (u_name.Text != "")
+            {
+                u_name.Background = new SolidColorBrush(white);
+                u_name.Foreground = new SolidColorBrush(black);
+                string name = u_name.Text;
+                name.Trim();
+                name = name.Substring(0, 1).ToUpper() + name.Substring(1).ToLower();
+                name.Replace(" ", "");
+                u_name.Text = name;
+                isNameCorrect = true;
+
+            }
+            else
+            {
+                u_name.Background = new SolidColorBrush(red);
+                u_name.Foreground = new SolidColorBrush(white);
+                isNameCorrect = false;
+            }
+
+            if (u_sname.Text != "")
+            {
+                u_sname.Background = new SolidColorBrush(white);
+                u_sname.Foreground = new SolidColorBrush(black);
+                string sname = u_sname.Text;
+                sname.Trim();
+                sname = sname.Substring(0, 1).ToUpper() + sname.Substring(1).ToLower();
+                sname.Replace(" ", "");
+                u_sname.Text = sname;
+
+            }
+
+            if (u_lname.Text != "")
+            {
+                u_lname.Background = new SolidColorBrush(white);
+                u_lname.Foreground = new SolidColorBrush(black);
+                string lname = u_lname.Text;
+                lname.Trim();
+                lname = lname.Substring(0, 1).ToUpper() + lname.Substring(1).ToLower();
+                lname.Replace(" ", "");
+                u_lname.Text = lname;
+                isLnameCorrect = true;
+
+            }
+            else
+            {
+                u_lname.Background = new SolidColorBrush(red);
+                u_lname.Foreground = new SolidColorBrush(white);
+                isLnameCorrect = false;
+            }
+
+            if (u_phone.Text != "")
+            {
+                u_phone.Background = new SolidColorBrush(white);
+                u_phone.Foreground = new SolidColorBrush(black);
+                string phone = u_phone.Text;
+                phone.Trim();
+                phone.Replace(" ", "");
+                if (phone.Length == 9)
+                {
+                    phone = "+48" + phone;
+                }
+                u_phone.Text = phone;
+            }
+
+            if (u_address.Text != "")
+            {
+                u_address.Background = new SolidColorBrush(white);
+                u_address.Foreground = new SolidColorBrush(black);
+                string address = u_address.Text;
+                address.Trim();
+                address = address.Substring(0, 1).ToUpper() + address.Substring(1).ToLower();
+                u_address.Text = address;
+                isAddressCorrect = true;
+            }
+            else
+            {
+                u_address.Background = new SolidColorBrush(red);
+                u_address.Foreground = new SolidColorBrush(white);
+                isAddressCorrect = false;
+            }
+
+            if (u_city.Text != "")
+            {
+                u_city.Background = new SolidColorBrush(white);
+                u_city.Foreground = new SolidColorBrush(black);
+                string city = u_city.Text;
+                city.Trim();
+                city = city.Substring(0, 1).ToUpper() + city.Substring(1).ToLower();
+                u_city.Text = city;
+                isCityCorrect = true;
+            }
+            else
+            {
+                u_city.Background = new SolidColorBrush(red);
+                u_city.Foreground = new SolidColorBrush(white);
+                isCityCorrect = false;
+            }
+
+            if (u_postcode.Text != "")
+            {
+                u_postcode.Background = new SolidColorBrush(white);
+                u_postcode.Foreground = new SolidColorBrush(black);
+                string postcode = u_postcode.Text;
+                postcode.Trim();
+                u_postcode.Text = postcode;
+                isPostcodeCorrect = true;
+            }
+            else
+            {
+                u_postcode.Background = new SolidColorBrush(red);
+                u_postcode.Foreground = new SolidColorBrush(white);
+                isPostcodeCorrect = false;
+            }
+            if (isPeselFilled == true && isNameCorrect == true && isLnameCorrect == true && isDateCorrect == true && isAddressCorrect == true && isCityCorrect == true && isPostcodeCorrect == true)
+            {
+                if(isPeselCorrect == true)
+                {
+                    InputDataReturned = true;
+                    this.Close();
+                } else
+                {
+                    MessageBox.Show("Niepoprawny numer pesel!", "Błąd", MessageBoxButton.OK);
+                }
                 
             }
             else
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,11 +12,24 @@ using Microsoft.Win32;
 using System.Collections.ObjectModel;
 using System.IO;
 
+
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    class Osoba
+    {
+        public string? u_pesel { get; set; }
+    public string? u_name { get; set; }
+    public string? u_sname { get; set; }
+    public string? u_lname { get; set; }
+    public string? u_date { get; set; }
+    public string? u_phone { get; set; }
+    public string? u_address { get; set; }
+    public string? u_city { get; set; }
+    public string? u_postcode { get; set; }
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -34,8 +47,23 @@ namespace WpfApp1
         private void NewRecord_Click(object sender, RoutedEventArgs e)
         {
             InputWindow iW = new InputWindow();
-            iW.Show();
-            this.Close();
+            iW.ShowDialog();
+            if(iW.InputDataReturned == true)
+            {
+                Osoba uczen = new();
+                uczen.u_pesel = iW.u_pesel.Text;
+                uczen.u_name = iW.u_name.Text;
+                uczen.u_sname = iW.u_sname.Text;
+                uczen.u_lname = iW.u_lname.Text;
+                uczen.u_date = iW.u_date.Text;
+                uczen.u_phone = iW.u_phone.Text;
+                uczen.u_address = iW.u_address.Text;
+                uczen.u_city = iW.u_city.Text;
+                uczen.u_postcode = iW.u_postcode.Text;
+                listView.Items.Add(uczen);
+            }
+
+            
         }
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
@@ -43,10 +71,67 @@ namespace WpfApp1
         }
         private void Open_Click(object sender, RoutedEventArgs e)
         {
-            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            openFileDialog.Title = "Otwórz plik CSV";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                listView.Items.Clear();
+                string filePath = openFileDialog.FileName;
+                int selectedFilterIndex = openFileDialog.FilterIndex;
+                string delimiter = ";";
+                if (selectedFilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                Encoding encoding = Encoding.UTF8;
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadAllLines(filePath, encoding);
+                    foreach (var line in lines)
+                    {
+                        string[] columns = line.Split(delimiter);
+                        if (columns != null)
+                        {
+                            Osoba uczen = new();
+                            uczen.u_pesel = columns.ElementAtOrDefault(0);
+                            uczen.u_name = columns.ElementAtOrDefault(1);
+                            uczen.u_sname = columns.ElementAtOrDefault(2);
+                            uczen.u_lname = columns.ElementAtOrDefault(3);
+                            uczen.u_date = columns.ElementAtOrDefault(4);
+                            uczen.u_phone = columns.ElementAtOrDefault(5);
+                            uczen.u_address = columns.ElementAtOrDefault(6);
+                            uczen.u_city = columns.ElementAtOrDefault(7);
+                            uczen.u_postcode = columns.ElementAtOrDefault(8);
+                            listView.Items.Add(uczen);
+                        }
+                    }
+                }
+            }
+
         }
         private void Save_Click(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            saveFileDialog.Title = "Zapisz jako plik CSV";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string delimiter = ";";
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (Osoba item in listView.Items)
+                    {
+                        var row = $"{item.u_pesel}{delimiter}{item.u_name}{delimiter}{item.u_sname}{delimiter}{item.u_lname}{delimiter}{item.u_date}{delimiter}{item.u_phone}{delimiter}{item.u_address}{delimiter}{item.u_city}{delimiter}{item.u_postcode}";
+                        writer.WriteLine(row);
+                    }
+                }
+            }
         }
         private void RemoveSel_Click(object sender, RoutedEventArgs e)
         {
